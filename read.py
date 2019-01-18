b995656a526c8b262943b839342397bfd1402cda
Tom Rein <tomjrein@gmail.com>
2019-01-18 15:42:00 -0500

    got pca working

--- read.py
+++ read.py
@@ -5,33 +5,40 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
 
-results = []
-
 def standardize_data(matrix):
     mean = np.mean(matrix, axis=0)
-    print("mean", mean)
     std = np.std(matrix, axis=0, ddof=1)
-    print("std", std)
     return (matrix - mean) / std
+
+def project(cov):
+    w, v = LA.eig(cov)
+    idx = np.argsort(w)[::-1][0:2]
+
+    projection_matrix = []
+    for i in idx:
+        eig_vec = v[:,i]
+        projection_matrix.append(eig_vec)
+
+    return np.array(projection_matrix).transpose()
+
+results = []
 
 for root, dirs, files in os.walk("./yalefaces/yalefaces/"):
     for name in files[1:]:
         filename = os.path.join(root, name)
         im = Image.open(filename).resize((40, 40))
-        results.append(im.getdata())
+        results.append(list(im.getdata()))
 
-test = np.array([[4,1,2], [2,4,0], [2,3,-8], [3,6,0], [4,4,0], [9,10,1], [6,8,-2], [9,5,1], [8,7,10], [10,8,-5]])
-new_test = standardize_data(test)
-cov = np.cov(new_test, rowvar=False)
-w, v = LA.eig(cov)
 
-print("new_test", new_test[0])
-print("test", v[:,0])
+results = np.array(results)
+results = standardize_data(results)
+cov = np.cov(results, rowvar=False)
 
-print(np.matmul(new_test, v[:,0]))
+#test = np.array([[4,1,2], [2,4,0], [2,3,-8], [3,6,0], [4,4,0], [9,10,1], [6,8,-2], [9,5,1], [8,7,10], [10,8,-5]])
+#new_test = standardize_data(test)
+#cov = np.cov(new_test, rowvar=False)
 
-#results = np.array(results)
-#results = standardize_data(results)
-#print("results", results.shape)
-#cov = np.cov(results)
-#print("uhhhh", cov.shape)
+projection_matrix = project(cov)
+z = np.matmul(results, projection_matrix)
+plt.scatter(z[:,0], z[:,1])
+plt.show()
