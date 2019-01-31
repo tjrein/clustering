a3f3bd4e003c3d480693eb01978aead2dd518004
Tom Rein <tomjrein@gmail.com>
2019-01-31 13:54:22 -0500

    moving logic into pca

---
+++ pca.py
@@ -0,0 +1,27 @@
+from read import read_files, standardize_data
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import linalg as LA
+
+def display_pca(results, cov):
+    projection_matrix = project(cov, 2)
+    z = np.matmul(results, projection_matrix)
+    plt.scatter(z[:,0], z[:,1])
+    plt.show()
+
+def project(cov, k):
+    w, v = LA.eig(cov)
+    idx = np.argsort(w)[::-1][0:k]
+
+    projection_matrix = []
+    for i in idx:
+        eig_vec = v[:,i]
+        projection_matrix.append(eig_vec)
+
+    return np.array(projection_matrix).transpose()
+
+results = np.array(read_files())
+results = standardize_data(results)
+cov = np.cov(results, rowvar=False)
+
+display_pca(results, cov)
--- read.py
+++ read.py
@@ -5,7 +5,6 @@ import numpy as np
 def read_files():
     results = []
     for root, dirs, files in os.walk("./yalefaces/yalefaces/"):
-        print("files", files)
         for name in files:
             if name == "Readme.txt":
                 continue
